6a56a76f277bff9ade2115ce42c085cf89eb3099
George Luis Pagarigan <george.luis.pagarigan@gmail.com>
2025-04-30 12:39:41 +0200

    Initial commit Next JS Enterprise Template

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,43 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier", "storybook/recommended"],
+    ignorePatterns: ["/lib/proto-buf/tribune/**"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": "warn",
+      curly: ["error", "all"],
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["sibling", "parent"], "index"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "no-empty-interface": "off",
+      "@typescript-eslint/no-explicit-any": ["error", { fixToUnknown: true }],
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "no-unused-vars": "warn",
+      "prefer-const": "warn",
+      "prefer-template": "warn",
+      "sort-imports": [
+        "error",
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+        },
+      ],
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import BundleAnalyzer from "@next/bundle-analyzer"
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+const withBundleAnalyzer = BundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})
+
+export default withBundleAnalyzer(nextConfig)
